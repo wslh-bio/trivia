36259a8fbd6ff191e7e732c3c3c70df5f313076d
Kelsey Florek <kelseyflorek22@gmail.com>
2026-09-21 14:35:37 -0500

    github pages edit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    // Allows deploying to a GitHub Pages project subpath, e.g. /repo-name/
+    baseURL: process.env.NUXT_APP_BASE_URL || '/'
+  },
   runtimeConfig: {
     public: {
       // URL of the deployed Google Apps Script Web App that appends
